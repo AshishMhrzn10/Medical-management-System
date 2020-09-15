8b449db9b6b566b5ff5cff9a9562e23de8396a1c
Ashish Maharjan <amaharjan1032@gmail.com>
2020-09-16 00:11:32 +0545

    Homepage theme setup

---
+++ reactmedicalstorefrontend/src/components/MainComponent.js
@@ -0,0 +1,58 @@
+import React from "react";
+import GoogleFontLoader from "react-google-font-loader";
+import Overlay from "./Overlay";
+import Navbar from "./Navbar";
+import PageLoader from "./PageLoader";
+import Sidebar from "./Sidebar";
+import HomeComponent from "./HomeComponent";
+
+import "adminbsb-materialdesign/css/themes/all-themes.css";
+
+class MainComponent extends React.Component {
+  state = {
+    bodyClass: "theme-red ls-closed",
+    displayOverlay: "none",
+  };
+  onBarClick = () => {
+    if (this.state.bodyClass === "theme-red ls-closed overlay-open") {
+      this.setState({ bodyClass: "theme-red ls-closed" });
+      this.setState({ displayOverlay: "none" });
+    } else if (this.state.bodyClass === "theme-red ls-closed") {
+      this.setState({ bodyClass: "theme-red ls-closed overlay-open" });
+      this.setState({ displayOverlay: "block" });
+    }
+  };
+  render() {
+    if (window.screen.width > 1167) {
+      document.getElementById("root").className = "theme-red";
+    } else {
+      document.getElementById("root").className = this.state.bodyClass;
+    }
+    return (
+      <React.Fragment>
+        <GoogleFontLoader
+          fonts={[
+            {
+              font: "Roboto",
+              weights: [400, 700],
+            },
+          ]}
+          subsets={["latin", "cyrillic-ext"]}
+        />
+        <GoogleFontLoader
+          fonts={[
+            {
+              font: "Material+Icons",
+            },
+          ]}
+        />
+        <Overlay display={this.state.displayOverlay} />
+        <Navbar onBarClick={this.onBarClick} />
+        <Sidebar />
+        <HomeComponent />
+      </React.Fragment>
+    );
+  }
+}
+
+export default MainComponent;
