7e42d8f2988c708139570461368fb0f013793b20
Ashish Maharjan <amaharjan1032@gmail.com>
2020-09-17 20:24:29 +0545

    Add company form

---
+++ reactmedicalstorefrontend/src/utils/APIHandler.js
@@ -0,0 +1,27 @@
+import AuthHandler from "./AuthHandler";
+import Axios from "axios";
+import Config from "./Config";
+
+class APIHandler {
+  async checkLogin() {
+    if (AuthHandler.checkTokenExpiry) {
+      var response = await Axios.post(Config.refreshApiUrl, {
+        refresh: AuthHandler.getRefreshToken(),
+      });
+      console.log(response);
+    }
+  }
+
+  async saveCompanyData(
+    name,
+    license_no,
+    address,
+    contact_no,
+    email,
+    description
+  ) {
+    this.checkLogin();
+  }
+}
+
+export default APIHandler;
